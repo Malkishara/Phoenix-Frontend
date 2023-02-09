6d9e5d1f4a1b3ec43f028e489ebefce6ec0b4b1d
Malkishara <isurumalkishara97@gmail.com>
2023-02-10 01:06:28 +0530

    show more error fixed

--- src/app/services/job-vacancies.service.ts
+++ src/app/services/job-vacancies.service.ts
@@ -25,82 +25,10 @@ types(){
   return this.http.get(this.url+"/types")
 }
 
-//get hr vacancies
-hrVacancies(){
-  return this.http.get(this.url+"/vacancy/hr")
+//get job modality
+modality(){
+  return this.http.get(this.url+"/modality")
 }
 
-//get account vacancies
-accountVacancies(){
-  return this.http.get(this.url+"/vacancy/account")
-}
 
-//get banking vacancies
-bankingVacancies(){
-  return this.http.get(this.url+"/vacancy/banking")
-}
-
-//get marketing vacancies
-marketingVacancies(){
-  return this.http.get(this.url+"/vacancy/marketing")
-}
-
-//get engineering vacancies
-engineerVacancies(){
-  return this.http.get(this.url+"/vacancy/engineering")
-}
-
-//get hospital vacancies
-hospitalVacancies(){
-  return this.http.get(this.url+"/vacancy/hospital")
-}
-
-//get hotels vacancies
-hotelsVacancies(){
-  return this.http.get(this.url+"/vacancy/hotels")
-}
-//get teaching vacancies
-teachVacancies(){
-  return this.http.get(this.url+"/vacancy/teaching")
-}
-
-//get environment vacancies
-environmentVacancies(){
-  return this.http.get(this.url+"/vacancy/environment")
-}
-
-//get remote vacancies
-remoteVacancies(){
-  return this.http.get(this.url+"/modality/remote")
-}
-
-//get office vacancies
-officeVacancies(){
-  return this.http.get(this.url+"/modality/office")
-}
-
-//get part time vacancies
-partTimeVacancies(){
-  return this.http.get(this.url+"/type/part_time")
-}
-
-//get full time vacancies
-fullTimeVacancies(){
-  return this.http.get(this.url+"/type/full_time")
-}
-
-//get intern vacancies
-internVacancies(){
-  return this.http.get(this.url+"/type/intern")
-}
-
-//get casual vacancies
-casualVacancies(){
-  return this.http.get(this.url+"/type/casual")
-}
-
-//get contract vacancies
-contractVacancies(){
-  return this.http.get(this.url+"/type/contract")
-}
 }
--- src/app/vacancies/vacancies.component.ts
+++ src/app/vacancies/vacancies.component.ts
@@ -7,18 +7,16 @@ import { JobVacanciesService } from '../services/job-vacancies.service';
   styleUrls: ['./vacancies.component.css']
 })
 export class VacanciesComponent {
-  categoryData:any;
   jobCategories:any;//for use categories data
   vacancyData:any;
-  allData:any;//for use vacancies data
+  allVacancyData:any;//for use vacancies data
   categorySelected:any;//for select category
-  types:any;
   jobTypes:any;//for show job types
+  jobModality:any;//for show job modality
   typeSelected:any;//for select type
+  vacancySelected?:any;//for more datails
 
 
-
-  //ReadMore:boolean=true;//for show & hide more details
   visible:boolean=false;//hide more details
 
   //for pagination
@@ -33,38 +31,43 @@ export class VacanciesComponent {
     this.postCategories();
     this.postVacancyData();
     this.postJobTypes();
+    this.postJobModality();
   }
 
   //fetch categories
   postCategories():void{
   this.categories.categories().subscribe((data)=>{
-    this.categoryData=data;
-    this.jobCategories=this.categoryData.categories;
+    this.jobCategories=data;
     console.warn(this.jobCategories);
     this.categorySelected=null;
   });
   }
 
 
+
    //fetch vacancies
   postVacancyData():void{
-
-
-
     this.categories.vacancies().subscribe((data)=>{
       this.vacancyData=data;
-      this.allData=this.vacancyData.vacancies;
-      console.warn(this.allData);
-      console.warn(this.allData.length);
+      this.allVacancyData=this.vacancyData
+      console.warn(this.allVacancyData);
+
     });
   }
 
   //post job type
   postJobTypes():void{
     this.categories.types().subscribe((data)=>{
-      this.types=data;
-      this.jobTypes=this.types.job_type;
+      this.jobTypes=data;
       console.warn(this.jobTypes);
+    });
+  }
+
+  //post job modality
+  postJobModality():void{
+    this.categories.modality().subscribe((data)=>{
+      this.jobModality=data;
+      console.warn(this.jobModality);
     });
   }
 
@@ -83,208 +86,18 @@ export class VacanciesComponent {
   //pagination end
 
   //show and hide description
-  onClick(id:any){
+  onClick(data:any):void{
 
-     console.warn(id)
-    for(let i:number=0;i<this.allData.length;i++){
+     console.warn(data)
+     this.vacancySelected=data;
+     this.visible=!this.visible;
 
-      if(id==this.allData[i].vacancy_id ){
-        console.warn(this.allData[i].description);
-        //this.ReadMore=!this.ReadMore;
-        this.visible=!this.visible;
-
-      }
-    }
   }
 
-  //select data according to the choosen category
-  selectCategory(catId:any){
-    console.warn(catId);
-    if(catId==="001"){
-      console.warn("accout")
-      this.categories.accountVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="002"){
-      console.warn("banking")
-      this.categories.bankingVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-    else if(catId==="003"){
-      console.warn("hr")
-      this.categories.hrVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="004"){
-      console.warn("marketing")
-      this.categories.marketingVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="005"){
-      console.warn("engineer")
-      this.categories.engineerVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="006"){
-      console.warn("hospital")
-      this.categories.hospitalVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="007"){
-      console.warn("hotel")
-      this.categories.hotelsVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="008"){
-      console.warn("teach")
-      this.categories.teachVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(catId==="009"){
-      console.warn("environment")
-      this.categories.environmentVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
 
-     else{
-      console.warn("false")
-      this.categories.vacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
+
 
 
   }
 
-  onClickCompany(c_id:any){
-    console.warn(c_id);
-  }
-//Select vacancies by type
-  selectType(t_id:any){
-     if(t_id=="T001"){
-      console.warn("full time")
-      this.categories.fullTimeVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
 
-     else if(t_id=="T002"){
-      console.warn("part time")
-      this.categories.partTimeVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(t_id=="T003"){
-      console.warn("intern")
-      this.categories.internVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(t_id=="T004"){
-      console.warn("casual")
-      this.categories.casualVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(t_id=="T005"){
-      console.warn("contract")
-      this.categories.contractVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else{
-      console.warn("false")
-      this.categories.vacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-  }
-
-  //select by modality
-  selectModality(m_id:any){
-     if(m_id=="1000"){
-      console.warn("office")
-      this.categories.officeVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-     else if(m_id=="1001"){
-      console.warn("remote")
-      this.categories.remoteVacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-    else{
-      console.warn("false")
-      this.categories.vacancies().subscribe((data)=>{
-        this.vacancyData=data;
-        this.allData=this.vacancyData.vacancies;
-        console.warn(this.allData);
-        console.warn(this.allData.length);
-      });
-     }
-  }
-}
